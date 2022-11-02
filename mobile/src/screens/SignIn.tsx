1798192c21ada62e82fe9786a680b553b8191707
italosaager <italosaagergmail.com>
2022-11-02 17:21:17 -0300

    Setup mobile

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import { StatusBar, Center, Text } from "native-base";
+import React from "react";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>
+        SignIn
+      </Text>
+      <StatusBar />
+    </Center>
+  );
+}
